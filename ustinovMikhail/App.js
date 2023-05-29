8378c2d39672b33b47df68fc15b62abe3022dd8b
Ustinov Mikhail <uncmishu@gmail.com>
2023-05-30 03:55:43 +0900

    move labs to components folder

--- ustinovMikhail/App.js
+++ ustinovMikhail/App.js
@@ -1,20 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Lab1 from './components/Lab1.js';
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>ez lab0 by Ustinov Mikhail</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Lab1 />
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
